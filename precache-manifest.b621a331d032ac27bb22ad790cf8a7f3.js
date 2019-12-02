1bfc1e89feb0f348a473cf8d43a43107051ccbb7
Bethan <bethancdavies@gmail.com>
2019-12-02 13:46:57 -0500

    Updates

---
+++ precache-manifest.b621a331d032ac27bb22ad790cf8a7f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f68cc803411de487a40c8845df37f81d",
+    "url": "/GroceryListApp/index.html"
+  },
+  {
+    "revision": "e1ba55da8cd0186fc41d",
+    "url": "/GroceryListApp/static/css/main.a34ad884.chunk.css"
+  },
+  {
+    "revision": "0553f604d24daab57bd2",
+    "url": "/GroceryListApp/static/js/2.e1e9c69d.chunk.js"
+  },
+  {
+    "revision": "e1ba55da8cd0186fc41d",
+    "url": "/GroceryListApp/static/js/main.25a2b877.chunk.js"
+  },
+  {
+    "revision": "24c1928c61c2535291f9",
+    "url": "/GroceryListApp/static/js/runtime-main.236af00c.js"
+  },
+  {
+    "revision": "9260d1f75fa7fda12bf6c70eb0049b27",
+    "url": "/GroceryListApp/static/media/7546.9260d1f7.jpg"
+  }
+]);
